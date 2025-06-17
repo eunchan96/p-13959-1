f830461b26834b07662079cec86fbb3331664683
eunchan96 <taegwon02@gmail.com>
2025-06-17 11:51:38 +0900

    entity(Question, Answer)

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,31 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
